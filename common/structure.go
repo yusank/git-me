a98130923e22339ac28816714cba38e08b2163fd
yusank <yusankurban@gmail.com>
2018-05-04 14:55:06 +0800

    fix

--- common/structure.go
+++ common/structure.go
@@ -1,21 +1,21 @@
 package common
 
 type URLData struct {
-	URL  string
-	Size int64
-	Ext  string
+	URL  string `json:"url"`
+	Size int64  `json:"size"`
+	Ext  string `json:"ext"`
 }
 
 // VideoData data struct of video info
 type VideoData struct {
-	Site      string
-	Title     string
-	OutputDir string
+	Site      string `json:"site"`
+	Title     string `json:"title"`
+	OutputDir string `json:"-"`
 	// [URLData: {URL, Size, Ext}, ...]
 	// Some video files have multiple fragments
 	// and support for downloading multiple image files at once
-	Formats []FormatData
-	Type    string
+	Formats []FormatData `json:"formats"`
+	Type    string       `json:"type"`
 }
 
 // FormatData data struct of every format
@@ -23,7 +23,7 @@ type FormatData struct {
 	// [URLData: {URL, Size, Ext}, ...]
 	// Some video files have multiple fragments
 	// and support for downloading multiple image files at once
-	URLs    []URLData
-	Quality string
-	Size    int64 // total size of all urls
+	URLs    []URLData `json:"urls"`
+	Quality string    `json:"quality"`
+	Size    int64     `json:"size"` // total size of all urls
 }
