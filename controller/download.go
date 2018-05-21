b5beb3da9ddf502c3b4628b540794f3342e21ab5
yusank <yusankurban@gmail.com>
2018-05-21 10:51:00 +0800

    temp

--- controller/download.go
+++ controller/download.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
-	"encoding/json"
-
+	"fmt"
 	"git-me/consts"
 	"git-me/extractors"
 	"git-me/models"
+	"git-me/utils"
+	"io"
+	"os"
 )
 
 type DownloaderController struct {
@@ -18,11 +20,12 @@ type DownloadInfo struct {
 
 func (dc *DownloaderController) ParseVideo() {
 	var di DownloadInfo
-	if err := json.Unmarshal(dc.Ctx.Input.RequestBody, &di); err != nil {
-		dc.OnError(err)
-		return
-	}
+	//if err := json.Unmarshal(dc.Ctx.Input.RequestBody, &di); err != nil {
+	//	dc.OnError(err)
+	//	return
+	//}
 
+	di.URL = "https://www.bilibili.com/video/av23606332/?spm_id_from=333.334.bili_game.4"
 	uid := dc.GetSession(consts.SessionUserID)
 	if uid == nil {
 		ip := dc.GetIp()
@@ -42,6 +45,26 @@ func (dc *DownloaderController) ParseVideo() {
 	if err != nil {
 		dc.OnError(err)
 		return
+	}
+
+	//dc.Ctx.ResponseWriter.Header().Set("Content-Type", "application/octet-stream")
+	//dc.Ctx.ResponseWriter.Header().Set("Content-Disposition", fmt.Sprintf("attachment;filename=a.mp4"))
+	//dc.Ctx.ResponseWriter.Header().Set("Content-Length", fmt.Sprintf("%d", 1024000))
+
+	file, _ := os.Create("temp.flv")
+	header := map[string]string{}
+	if len(vid.Formats) > 0 {
+		for _, v := range vid.Formats[0].URLs {
+			header["Referer"] = v.URL
+			resp, err := utils.HttpGet(v.URL, header)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Println(err)
+			writer := io.MultiWriter(file)
+			io.Copy(writer, resp.Body)
+		}
 	}
 
 	dc.JSON(vid)
